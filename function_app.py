477e794e60fc11645d300ffeb9a39f4fed507f37
Ian <ianolpx@gmail.com>
2024-12-22 00:04:04 +0900

    hotfix: change the crontab setting

--- function_app.py
+++ function_app.py
@@ -1,6 +1,7 @@
 import logging
 import azure.functions as func
 import asyncio
+import time
 from handlers.core import execute
 
 app = func.FunctionApp()
@@ -8,7 +9,7 @@ app = func.FunctionApp()
 
 # schedule="49 23 * * *",
 @app.schedule(
-    schedule="53 3,7,11,15,19,23 * * *",
+    schedule="57 * * * *",
     arg_name="myTimer",
     run_on_startup=True,
     use_monitor=False)
@@ -22,8 +23,15 @@ async def timer_trigger(myTimer: func.TimerRequest) -> None:
 
 
 async def test():
-    execute_handler = execute.ExecuteHandler()
-    await execute_handler.run()
+    hour = time.localtime().tm_hour
+    # print(hour)
+    if hour in [3, 7, 11, 15, 19, 23]:
+        execute_handler = execute.ExecuteHandler()
+        await execute_handler.run()
+    else:
+        logging.info('The timer is not in the list!')
+    # execute_handler = execute.ExecuteHandler()
+    # await execute_handler.run()
 
 # python -m function_app
 if __name__ == '__main__':
