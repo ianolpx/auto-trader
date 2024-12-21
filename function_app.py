623635d74e9cf1474390af1c29f65ae42afaef3c
Ian <ianolpx@gmail.com>
2024-12-21 23:07:04 +0900

    feat: 1d -> 4h

--- function_app.py
+++ function_app.py
@@ -6,8 +6,9 @@ from handlers.core import execute
 app = func.FunctionApp()
 
 
+# schedule="49 23 * * *",
 @app.schedule(
-    schedule="49 23 * * *",
+    schedule="53 3,7,11,15,19,23 * * *",
     arg_name="myTimer",
     run_on_startup=True,
     use_monitor=False)
